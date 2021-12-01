9938aeb352c11e5065f91a5ae3e7a5737c8059d2
Alex <prof.alexresende@gmail.com>
2021-12-01 12:12:21 -0300

    Alterações no código

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -16,5 +16,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnSomar.setOnClickListener{somar()}
+    }
+
+    fun somar(){
+        val n1 = (binding.edtN1.text).toString().toFloat()
+        val n2 = (binding.edtN2.text).toString().toFloat()
+        val res = n1+n2
+        binding.txvResulado.text="%.2f".format(res)
     }
 }
